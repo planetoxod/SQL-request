02f3cfcefa3be2baaaafa816c29bd9bbb4391fbc
Viktor Furmanov <52571064+planetoxod@users.noreply.github.com>
2019-09-26 17:43:05 +0300

    SQL query
    SQL query to select all product Name – category name pairs

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿SELECT  TableProduct.Продукт, TableProduct.Категория1
+FROM  TableProduct 
+ SELECT  TableProduct.Продукт, TableProduct.Категория2
+FROM  TableProduct  
+SELECT  TableProduct.Продукт, TableProduct.Категория3
+FROM  TableProduct 
+SELECT  Продукт1, TableCategory.Категория
+FROM     TableCategory
+SELECT  Продукт2, TableCategory.Категория
+FROM     TableCategory
+SELECT  Продукт3, TableCategory.Категория
+FROM     TableCategory
